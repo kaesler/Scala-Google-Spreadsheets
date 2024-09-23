f467681a90753eec5efc69ea02a741df9207169d
Kevin Esler <kevin.esler@gmail.com>
2024-09-23 14:18:19 +1000

    Use fold on Try

--- src/main/scala/gsheets/cells/GSheetGrid.scala
+++ src/main/scala/gsheets/cells/GSheetGrid.scala
@@ -6,7 +6,7 @@ import scala.scalajs.js.JSConverters.*
 type GSheetGrid = JsGrid[GSheetCellValue]
 
 object GSheetGrid:
-  def one(cv: GSheetCellValue): GSheetGrid = js.Array(js.Array(cv))
+  def one(cv: GSheetCellValue): GSheetGrid = JsGrid.one(cv)
 
   def apply(cells: ScalaCellGrid): GSheetGrid =
     cells.map(_.map(_.value).toJSArray).toJSArray
--- src/main/scala/gsheets/customfunctionsimpl/CustomFunction0.scala
+++ src/main/scala/gsheets/customfunctionsimpl/CustomFunction0.scala
@@ -10,16 +10,16 @@ final class CustomFunction0[+U](f: () => U)(
 ) extends (() => GSheetGrid) {
 
   def apply(): GSheetGrid = {
-    Try(decoder(f())) match {
-      case Success(value) => value
-      case Failure(e)     => js.Array(js.Array(e.getMessage))
-    }
+    Try(decoder(f()))
+      .fold(
+        ex => GSheetGrid.one(ex.getMessage),
+        identity
+      )
   }
-
 }
 
 object CustomFunction0 {
 
-  extension[U: Decoder] (f: () => U)
+  extension [U: Decoder](f: () => U)
     def asCustomFunction: CustomFunction0[U] = CustomFunction0(f)
 }
