105e8e8cd7ab5bc4dbe7af79b806a3df3a01c64a
Kevin Esler <kevin.esler@gmail.com>
2024-09-25 13:34:39 +1000

    Use Scala 3 union types

--- src/main/scala/gsheets/cells/GSheetCellValue.scala
+++ src/main/scala/gsheets/cells/GSheetCellValue.scala
@@ -1,7 +1,6 @@
 package gsheets.cells
 
 import scala.scalajs.js
-import scala.scalajs.js.|
 
 /** The type of Data that we can receive from Google SpreadSheets. */
 type GSheetCellValue = String | Double | Boolean | js.Date
--- src/main/scala/gsheets/customfunctions/Input.scala
+++ src/main/scala/gsheets/customfunctions/Input.scala
@@ -2,6 +2,5 @@ package gsheets.customfunctions
 
 import gsheets.cells.{GSheetCellValue, GSheetGrid}
 import scala.scalajs.js
-import scala.scalajs.js.|
 
 type Input = GSheetCellValue | GSheetGrid
